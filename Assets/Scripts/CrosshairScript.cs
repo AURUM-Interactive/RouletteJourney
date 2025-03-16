2bfaa4a6f0a135e3c2dbfc8d824f99b30912eeaf
Arnas <105560627+shush2u@users.noreply.github.com>
2025-03-16 22:12:54 +0200

    Implemented cursor color change when hovering over objects

--- Assets/Scripts/CrosshairScript.cs
+++ Assets/Scripts/CrosshairScript.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class CrosshairScript : MonoBehaviour
 {
+    SpriteRenderer cursorSprite;
+
+    BoxCollider2D boxCollider;
+
     Vector2 mousePosition;
     void Awake()
     {
         Cursor.visible = false;
+        boxCollider = GetComponent<BoxCollider2D>();
+        cursorSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -13,5 +19,18 @@ public class CrosshairScript : MonoBehaviour
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mousePosition;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            cursorSprite.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        cursorSprite.color = Color.white;
     }
 }
