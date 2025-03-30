14def21d710ffa65dad18166e4b7d6bb8a4316c9
Arnas <105560627+shush2u@users.noreply.github.com>
2025-03-30 22:44:57 +0300

    Implemented UI for poker chip currency

--- Assets/Scripts/CurrencyLootScript.cs
+++ Assets/Scripts/CurrencyLootScript.cs
@@ -14,7 +14,7 @@ public class CurrencyLootScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) {
-            PlayerMain.currencyChipCount++;
+            PlayerMain.chipCount++;
             Destroy(this.gameObject);
         }
     }
