a97d26761aadac266fb3fefa35cf1741710d0237
Arnas <105560627+shush2u@users.noreply.github.com>
2025-04-29 00:25:45 +0300

    Death Screen Functionality

--- Assets/Scripts/UI/GameOverMenuScript.cs
+++ Assets/Scripts/UI/GameOverMenuScript.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class GameOverMenuScript : MonoBehaviour
 {
-
-    public Button RestartButton;
-    public Button QuitToMainMenuButton;
     public void Setup()
     {
         gameObject.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
