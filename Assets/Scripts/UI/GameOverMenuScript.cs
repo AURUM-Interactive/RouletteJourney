1eeee135f08e8e0bb20c554c5de2110fd1805ede
Arnas <105560627+shush2u@users.noreply.github.com>
2025-04-29 00:13:26 +0300

    Death Screen Visuals Implemented

---
+++ Assets/Scripts/UI/GameOverMenuScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class GameOverMenuScript : MonoBehaviour
+{
+
+    public Button RestartButton;
+    public Button QuitToMainMenuButton;
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+}
